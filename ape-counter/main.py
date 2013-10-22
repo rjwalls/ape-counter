f0a587f475366615223cab3440383290460822d3
Robert Walls <rjwalls@cs.umass.edu>
2013-10-22 11:06:13 -0400

    Lot's of exploration improvements.
    I made a bunch of changes while trying to implement the exploration component for the Counter protocol. The explore protocol now works, and it uses an updated method to guess the current state from the model. I also added the protocol factory and a simple driver class.
    
    I also updated the Counter protocol to include payload lengths.

--- ape-counter/main.py
+++ ape-counter/main.py
@@ -1,21 +1,30 @@
+#!/usr/bin/env python2.7
 __author__ = 'wallsr'
 
+import struct
+
 from ape.protocol.explore import ExploreProtocol
+from ape.protocol.factory import ProtocolFactory
+from ape.protocol.driver import TesterDriver
+
 
 class TesterProtocol(ExploreProtocol):
     def send_hello(self):
-        self.transport.write('hello')
+        self.transport.write(struct.pack('!I', 5) + 'hello')
 
     def send_ping(self):
-        self.transport.write('ping')
+        self.transport.write(struct.pack('!I', 4) + 'ping')
 
     def send_goodbye(self):
-        self.transport.write('goodbye')
+        self.transport.write(struct.pack('!I', 7) + 'goodbye')
 
-    def handle_new_client(self):
+    def handle_new_client(self, data):
         pass
 
     def get_message_type(self, message):
         #Use this method to determine message type
         return "new_client"
 
+
+driver = TesterDriver(TesterProtocol)
+driver.run()
--- counter/server.py
+++ counter/server.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python2.7
 __author__ = 'wallsr'
+import struct
 
 
 from twisted.internet import protocol, reactor
@@ -9,32 +11,53 @@ class CounterServer(protocol.Protocol):
         self.connected_peers = []
         self.new_peers = []
         self.factory = factory
+        self.data = ''
 
     def connectionMade(self):
         self.factory.clients.add(self)
+        self._handle_data = self.handle_data()
+        self._next_length = self._handle_data.next()
 
-    def dataReceived(self, message):
-        #Remove the newlines
-        message = message.strip()
-        IP = self.transport.getHost().host
+    def dataReceived(self, data):
+        data = self.data + data
+        next_length = self._next_length
 
-        if IP not in self.connected_peers:
-            self.new_peers.append(IP)
+        while len(data) >= next_length:
+            data_send, data = data[:next_length], data[next_length:]
+            next_length = self._handle_data.send(data_send)
 
-        if message == "ping":
-            self.handle_ping(IP)
-        elif message == "hello":
-            print 'Hello from: %s' %IP
-            self.connected_peers.append(IP)
-            self.new_peers.append(IP)
-        elif message == "goodbye":
-            self.connected_peers.remove(IP)
-        else:
-            pass
+        self.data = data
+        self._next_length = next_length
+
+    def handle_data(self):
+        while True:
+            size, = struct.unpack('!I', (yield 4))
+            message = yield size
+            #Remove the newlines
+            message = message.strip()
+            IP = self.transport.getHost().host
+
+            print '%s from: %s' % (message, IP)
+
+            if IP not in self.connected_peers:
+                self.new_peers.append(IP)
+
+            if message == "ping":
+                self.handle_ping(IP)
+            elif message == "hello":
+                self.connected_peers.append(IP)
+                self.new_peers.append(IP)
+            elif message == "goodbye":
+                self.connected_peers.remove(IP)
+            else:
+                pass
 
     def handle_ping(self, IP):
+        client_str = str(';'.join(self.new_peers))
+
         for client in self.factory.clients:
-            client.transport.write(';'.join(self.new_peers))
+            length = struct.pack('!I', len(client_str))
+            client.transport.write(length + client_str)
 
         self.new_peers = []
 
